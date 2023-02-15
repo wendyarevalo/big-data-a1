f333efca919eb04ea54b66fc9ac1f3994ff04626
Wendy Arevalo <wendyyunuen@hotmail.com>
2023-02-15 12:04:36 +0200

    modify rabbitmq producer json message

--- code/dataingest/producer.py
+++ code/dataingest/producer.py
@@ -1,5 +1,6 @@
 import csv
 import pika
+import json
 
 with open('../../data/small_sample_reddit_comments.csv', 'r') as csvfile:
     csvreader = csv.reader(csvfile)
@@ -12,8 +13,18 @@ with open('../../data/small_sample_reddit_comments.csv', 'r') as csvfile:
     channel.queue_declare(queue='comments')
 
     for row in csvreader:
-        filtered_row = ','.join([row[0], row[1], row[8], row[9], row[14], row[15], row[17]])
-        print(filtered_row)
-        channel.basic_publish(exchange='', routing_key='comments', body=filtered_row)
+        filtered_row = [row[0], row[1], row[8], row[9], row[14], row[15], row[17]]
+
+        data = {
+            'created_utc': filtered_row[0],
+            'ups': filtered_row[1],
+            'subreddit': filtered_row[2],
+            'id': filtered_row[3],
+            'author': filtered_row[4],
+            'score': filtered_row[5],
+            'body': filtered_row[6]
+        }
+        message = json.dumps(data)
+        channel.basic_publish(exchange='', routing_key='comments', body=message)
 
     connection.close()
